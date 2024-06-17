df98cd83b5a8190babdd8fa8a72b974d4a5ebbad
Anna Zocher <anna.zocher@gmail.com>
2024-06-16 20:26:05 -0700

    add blank content for boilerplate

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 document
   .getElementById("generateButton")
   .addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: generateRandomEmoji,
-      });
-    });
+    generateRandomEmoji();
   });
 
 function generateRandomEmoji() {
