d2aa56a4af82c20550a2d5220f19177e1a3ef575
Anna Zocher <anna.zocher@gmail.com>
2024-06-16 19:57:38 -0700

    add emoji add info

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// Script that handles chrome extension actions
+document
+  .getElementById("generateButton")
+  .addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: generateRandomEmoji,
+      });
+    });
+  });
+
+function generateRandomEmoji() {
+  const emojiCodes = [
+    "&#x1F600;", // 😀 Grinning Face
+    "&#x1F604;", // 😄 Grinning Face With Smiling Eyes
+    "&#x1F609;", // 😉 Winking Face
+    "&#x1F60A;", // 😊 Smiling Face With Smiling Eyes
+    "&#x1F60D;", // 💙 Smiling Face With Heart-Eyes
+    "&#x1F923;", // 🤣 Rolling on the Floor Laughing
+    "&#x1F60E;", // 😎 Smiling Face With Sunglasses
+    "&#x1F618;", // 😘 Face Blowing a Kiss
+    "&#x1F970;", // 🥰 Smiling Face With Hearts
+    "&#x1F62D;", // 😭 Loudly Crying Face
+    "&#x1F631;", // 😱 Face Screaming in Fear
+    "&#x1F621;", // 😡 Pouting Face
+    "&#x1F92F;", // 🤯 Exploding Head
+    "&#x1F914;", // 🤔 Thinking Face
+    "&#x1F602;", // 😂 Face With Tears of Joy
+  ];
+
+  const randomEmoji = emojiCodes[Math.floor(Math.random() * emojiCodes.length)];
+
+  // add random emoji to doc
+  const emojiHolder = document.getElementById("emojiHolder");
+  emojiHolder.innerHTML = randomEmoji;
+}
